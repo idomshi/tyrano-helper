cd9d7eb25a101a02f259b11fd02b85ab2230c2b5
idomshi <56523771+idomshi@users.noreply.github.com>
2022-02-05 11:16:05 +0900

    Script converterをSFCにしました。

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello pages!</h1>
+        <ul>
+            <li>
+                <NuxtLink to="script-converter">Script Converter</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
